c72efaa2616a0ff419de207550d57a31d53c7e40
MohamadSafi <the.mohammad.safi@gmail.com>
2024-09-19 20:37:10 +0300

    fix crashing when server is off

--- client/services/message_service.py
+++ client/services/message_service.py
@@ -1,18 +1,43 @@
 import requests
 import uuid
+import time
+from requests.exceptions import ConnectionError
 
-server_url = 'http://localhost:5005'
+server_url = "http://localhost:5005"
 client_id = str(uuid.uuid4())
 
 
 def send_message_to_server(text):
-    response = requests.post(f'{server_url}/messages', json={'text': text, 'sender': client_id})
-    return 'Message sent successfully.' if response.status_code == 200 else 'Failed to send message.'
+    try:
+        start_time = time.time()
+        response = requests.post(
+            f"{server_url}/messages", json={"text": text, "sender": client_id}
+        )
+        elapsed_time = time.time() - start_time
+        print(f"Time taken to send message: {elapsed_time} seconds")
+        return (
+            "Message sent successfully."
+            if response.status_code == 200
+            else "Failed to send message."
+        )
+    except ConnectionError:
+        return "Connection to the server failed."
+
 
 def get_messages_from_server():
-    response = requests.get(f'{server_url}/messages')
-    return response.json() if response.status_code == 200 else []
+    try:
+        response = requests.get(f"{server_url}/messages")
+        return response.json() if response.status_code == 200 else []
+    except ConnectionError:
+        return []
+
 
 def get_message_count_from_server():
-    response = requests.get(f'{server_url}/messages/count')
-    return response.json().get('count') if response.status_code == 200 else None
+    try:
+        start_time = time.time()
+        response = requests.get(f"{server_url}/messages/count")
+        elapsed_time = time.time() - start_time
+        print(f"Time taken to get message count: {elapsed_time} seconds")
+        return response.json().get("count") if response.status_code == 200 else None
+    except ConnectionError:
+        return None
